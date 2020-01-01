2773c0d22b3c4d0e876e9b24b8dd2a6d39e5d151
shenzhonglin <835959138@qq.com>
2020-01-01 23:12:11 +0800

    函数和指针

---
+++ function/function.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+ int sum(){
+        return 10;
+    }
+int main(){
+   printf("%d",sum());
+    return 0;
+}
--- hellow.c
+++ hellow.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-int main()
-{
-    int price=8;
-    printf("请输入中文\n");//乱码问题详见 readme
-   scanf("%d",&price);
-   printf("%d" ,price);
-    return 0; 
+int sum(){
+    int i=0;
+    return i;
 }
- 
+int main(){
+    int price = 8;
+    int parsum=sum();
+    printf("请输入中文\n"); //乱码问题详见 readme
+    scanf("%d", &price);
+    printf("%d\n", price);
+    
+    printf("sum的值为%d",parsum);
+    return 0;
+}
+
